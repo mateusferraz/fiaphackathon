bfd231144804dbb870c68da79b1d2f50ed2a88af
Rodrigo Michel <rodrigo.micheld@gmail.com>
2024-08-02 18:28:18 -0300

    Finalizado os testes do agendamento paciente

---
+++ backend/src/Application/Interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entidades;
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IEmailService
+    {
+        void EnviarEmail(Paciente paciente, Agenda agenda);
+    }
+}
