48d97df4a2df32db099fa28f3ff4cb5c77cde1a3
ferreiraedilson <42985659+ferreiraedilson@users.noreply.github.com>
2024-08-01 21:53:46 -0300

    feature(Ajuste testes do médico)

--- backend/tests/UnitTests/Application/Services/Medicos/CadastrarMedicoRequestHandlerTest.cs
+++ backend/tests/UnitTests/Application/Services/Medicos/CadastrarMedicoRequestHandlerTest.cs
@@ -1,46 +1,61 @@
 ﻿
-using AutoMapper;
 using Application.Interfaces;
 using Moq;
-using System.Reflection.Metadata;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using System.Linq.Expressions;
 using Application.Services.Accounts;
 using Application.Requests.Medicos;
 using Domain.Entidades;
+using System.Reflection.Metadata;
+using Azure.Core;
 
 namespace UnitTests.Application.Services.Medicos
 {
     public class CadastrarMedicoRequestHandlerTest
-    {
+    {        
         private readonly Mock<IUnitOfWork> _mockUnitOfWork;
         private readonly CadastrarMedicoRequest _request;
         private readonly CadastrarMedicoRequestHandler _handler;
+        private readonly ILogger<CadastrarMedicoRequestHandler> _mockLogger;
 
         public CadastrarMedicoRequestHandlerTest()
-        {
+        {   
             _mockUnitOfWork = new Mock<IUnitOfWork>();
-            _mockUnitOfWork.Setup(s => s.MedicoRepository.SelectOne(It.IsAny<Expression<Func<Medico, bool>>>())).Returns(new Medico());
             _request = new CadastrarMedicoRequest();
             _handler = new CadastrarMedicoRequestHandler(                
-                Substitute.For<ILogger<CadastrarMedicoRequestHandler>>(),
-                _mockUnitOfWork.Object
-                );
+                Substitute.For<ILogger<CadastrarMedicoRequestHandler>>(), _mockUnitOfWork.Object);
+            _mockLogger = Substitute.For<ILogger<CadastrarMedicoRequestHandler>>();
         }
 
-        [Fact]        
-        public void Should_return_client_requested()
+        [Fact]
+        public void Should_return_Medico_requested() 
         {
-            var response = _handler.Handle(_request, CancellationToken.None);
+            var request = new CadastrarMedicoRequest
+            {
+                Crm = "0",
+                Documento="0",
+                Email="Teste@teste.com.br",
+                Nome="Teste da Silva",
+                Senha="123"
+            };
 
-            Assert.NotNull(response);
+            Medico medico = null;
+            _mockUnitOfWork.Setup(s => s.MedicoRepository
+                    .SelectOne(It.IsAny<Expression<Func<Medico, bool>>>()))
+                    .Returns(medico);
+
+            var handler = new CadastrarMedicoRequestHandler(_mockLogger, _mockUnitOfWork.Object);
+            Assert.ThrowsAsync<NullReferenceException>(() => handler.Handle(request, CancellationToken.None));            
         }
+
 
         [Fact]
         public void Should_return_NullReferenceException_when_client_not_found()
         {
-            _mockUnitOfWork.Setup(s => s.MedicoRepository.SelectOne(It.IsAny<Expression<Func<Medico, bool>>>())).Throws(new NullReferenceException());
+            _mockUnitOfWork.Setup(s => s.MedicoRepository
+                .SelectOne(It.IsAny<Expression<Func<Medico, bool>>>()))
+                .Throws(new NullReferenceException());
             Assert.ThrowsAsync<NullReferenceException>(() => _handler.Handle(_request, CancellationToken.None));
         }
 
